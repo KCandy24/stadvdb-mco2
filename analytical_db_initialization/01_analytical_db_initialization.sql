283345ca4383e4ec851e29595109b649fa467721
Roemer Caliboso <roemergabrielcalib@gmail.com>
2025-11-25 10:44:57 +0800

    added db initialization and batch updater

---
+++ analytical_db_initialization/01_analytical_db_initialization.sql
@@ -0,0 +1,100 @@
+CREATE SCHEMA IF NOT EXISTS analytical;
+
+CREATE TABLE IF NOT EXISTS analytical.dim_customer(
+    customer_id SERIAL PRIMARY KEY,
+    o_user_id INT,
+    lastname VARCHAR(50),
+    firstname VARCHAR(50),
+    birthday TIMESTAMP,
+    email VARCHAR(200)
+);
+
+CREATE TABLE IF NOT EXISTS analytical.dim_show(
+    show_id SERIAL PRIMARY KEY,
+    o_play_id INT,
+    o_showing_id INT,
+    basefee NUMERIC(10,2),
+    play_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS analytical.dim_venue(
+    venue_id SERIAL PRIMARY KEY,
+    o_theater_id INT,
+    o_seat_id INT,
+    theater_name VARCHAR(50),
+    location VARCHAR(50),
+    seat_column INT,
+    seat_row INT
+);
+
+CREATE TABLE IF NOT EXISTS analytical.dim_time (
+    time_id SERIAL PRIMARY KEY,
+    sale_date DATE,
+    day_number INT,
+    month_number INT,
+    quarter INT,
+    year INT
+);
+
+CREATE TABLE IF NOT EXISTS analytical.fact_sale (
+    sale_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    venue_id INT, 
+    show_id INT,
+    time_id INT,
+    ticket_price NUMERIC(10,2),
+    o_reservation_id INT,
+    
+    CONSTRAINT fk_show
+        FOREIGN KEY(show_id)
+        REFERENCES analytical.dim_show(show_id)
+        ON DELETE SET NULL,
+    
+    CONSTRAINT fk_customer
+        FOREIGN KEY(customer_id)
+        REFERENCES analytical.dim_customer(customer_id)
+        ON DELETE SET NULL,
+    
+    CONSTRAINT fk_venue
+        FOREIGN KEY(venue_id)
+        REFERENCES analytical.dim_venue(venue_id)
+        ON DELETE SET NULL,
+        
+    CONSTRAINT fk_time
+        FOREIGN KEY(time_id)
+        REFERENCES analytical.dim_time(time_id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS analytical.etl_log (
+    log_id SERIAL PRIMARY KEY,
+    last_run_time TIMESTAMP DEFAULT '1900-01-01 00:00:00'
+);
+
+INSERT INTO analytical.etl_log (last_run_time) 
+SELECT '1900-01-01 00:00:00' 
+WHERE NOT EXISTS (SELECT 1 FROM analytical.etl_log);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_dim_customer_oid ON analytical.dim_customer(o_user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_dim_show_oid ON analytical.dim_show(o_showing_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_dim_venue_oid ON analytical.dim_venue(o_seat_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_dim_time_date ON analytical.dim_time(sale_date);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_fact_sale_oid ON analytical.fact_sale(o_reservation_id);
+
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+DROP SERVER IF EXISTS transactional_server CASCADE;
+
+CREATE SERVER transactional_server
+    FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host 'transactional_db', port '5432', dbname 'appdb_transactional');
+
+CREATE USER MAPPING FOR postgres
+    SERVER transactional_server
+    OPTIONS (user 'postgres', password 'postgres');
+
+CREATE SCHEMA IF NOT EXISTS source_transactional;
+
+IMPORT FOREIGN SCHEMA transactional
+    FROM SERVER transactional_server
+    INTO source_transactional;
---
+++ analytical_db_initialization/02_create_procedure.sql
@@ -0,0 +1,75 @@
+CREATE OR REPLACE PROCEDURE analytical.batch_update_dw_incremental()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_last_run TIMESTAMP;
+    v_new_run_time TIMESTAMP;
+BEGIN
+
+    SELECT last_run_time INTO v_last_run FROM analytical.etl_log ORDER BY log_id DESC LIMIT 1;
+
+    v_new_run_time := NOW();
+
+    RAISE NOTICE 'Fetching data created after: %', v_last_run;
+
+    INSERT INTO analytical.dim_customer (o_user_id, lastname, firstname, birthday, email)
+    SELECT user_id, lastname, firstname, birthday, email
+    FROM source_transactional.users
+    ON CONFLICT (o_user_id) DO UPDATE 
+    SET lastname = EXCLUDED.lastname, email = EXCLUDED.email;
+
+    INSERT INTO analytical.dim_show (o_play_id, o_showing_id, basefee, play_name)
+    SELECT p.play_id, s.showing_id, s.basefee, p.play_name
+    FROM source_transactional.showing s
+    JOIN source_transactional.play p ON s.play_id = p.play_id
+    ON CONFLICT (o_showing_id) DO NOTHING;
+
+    INSERT INTO analytical.dim_venue (o_theater_id, o_seat_id, theater_name, location, seat_column, seat_row)
+    SELECT t.theater_id, s.seat_id, t.theater_name, t.location, s.seat_column, s.seat_row
+    FROM source_transactional.seat s
+    JOIN source_transactional.theater t ON s.theater_id = t.theater_id
+    ON CONFLICT (o_seat_id) DO NOTHING;
+
+
+    INSERT INTO analytical.dim_time (sale_date, day_number, month_number, quarter, year)
+    SELECT DISTINCT 
+        DATE(r.time_reserved),
+        EXTRACT(DAY FROM r.time_reserved),
+        EXTRACT(MONTH FROM r.time_reserved),
+        EXTRACT(QUARTER FROM r.time_reserved),
+        EXTRACT(YEAR FROM r.time_reserved)
+    FROM source_transactional.reservation r
+    WHERE r.time_reserved > v_last_run
+    ON CONFLICT (sale_date) DO NOTHING;
+
+
+    INSERT INTO analytical.fact_sale (
+        customer_id, venue_id, show_id, time_id, 
+        ticket_price, o_reservation_id
+    )
+    SELECT 
+        dc.customer_id,
+        dv.venue_id,
+        ds.show_id,
+        dt.time_id,
+        (ds.basefee + COALESCE(seat_src.pricing, 0)),
+        r.reservation_id
+    FROM source_transactional.reservation r
+    JOIN source_transactional.run run ON r.run_id = run.run_id
+    JOIN source_transactional.showing s ON run.showing_id = s.showing_id
+    JOIN source_transactional.seat seat_src ON r.seat_id = seat_src.seat_id
+
+    JOIN analytical.dim_customer dc ON r.user_id = dc.o_user_id
+    JOIN analytical.dim_show ds ON s.showing_id = ds.o_showing_id
+    JOIN analytical.dim_venue dv ON r.seat_id = dv.o_seat_id
+    JOIN analytical.dim_time dt ON DATE(r.time_reserved) = dt.sale_date
+
+    WHERE r.time_reserved > v_last_run
+    
+    ON CONFLICT (o_reservation_id) DO NOTHING;
+
+    UPDATE analytical.etl_log SET last_run_time = v_new_run_time;
+
+    RAISE NOTICE '> % records processed', v_new_run_time;
+END;
+$$;
