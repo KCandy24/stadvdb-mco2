f9af9f710a18f5fff054d74d07616066c73cabc9
Roemer Caliboso <roemergabrielcalib@gmail.com>
2025-11-27 01:09:19 +0800

    add batch reservation procedure

--- transactional_db_initialization/08_crud_reservations.sql
+++ transactional_db_initialization/08_crud_reservations.sql
@@ -1,3 +1,20 @@
--- Resevations
+CREATE OR REPLACE FUNCTION transactional.batch_create_reservation(
+    p_user_id INT,
+    p_run_id INT,
+    p_seat_ids INT[]
+)
+LANGUAGE plpgsql;
+RETURNS VOID AS $$
+BEGIN
+    INSERT INTO transactional.reservation (user_id, run_id, seat_id)
+    SELECT 
+        p_user_id, 
+        p_run_id, 
+        s 
+    FROM unnest(p_seat_ids) AS s
+    ORDER BY s ASC;
+END;
+$$
 
--- TODO
+
+
