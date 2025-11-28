bb9307cbf72a4478f790dca9048f261f35429e1d
Justin Rainier Go <81730861+jrgo7@users.noreply.github.com>
2025-11-28 14:29:46 +0800

    User dashboard and unreserve route connects to database

--- transactional_db_initialization/08_crud_reservations.sql
+++ transactional_db_initialization/08_crud_reservations.sql
@@ -1,3 +1,8 @@
+DROP PROCEDURE IF EXISTS transactional.batch_create_reservation (INT, INT, INT);
+
+DROP FUNCTION IF EXISTS transactional.read_reservations_of_user (integer);
+
+-- ! DON'T TOUCH - Roemer
 CREATE OR REPLACE PROCEDURE transactional.batch_create_reservation(
     p_user_id INT,
     p_run_id INT,
@@ -14,3 +19,34 @@ BEGIN
     ORDER BY s ASC;
 END;
 $$ LANGUAGE plpgsql
+-- ! End of DON'T TOUCH
+
+CREATE OR REPLACE FUNCTION transactional.read_reservations_of_user (p_user_id integer)
+RETURNS TABLE (
+    reservation_id int,
+    play_name varchar(50),
+    theater_name varchar(50),
+    seat int,
+    period_start timestamp,
+    period_end timestamp
+) 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        r.reservation_id,
+        p.play_name,
+        t.theater_name,
+        r.seat_id,
+        s.reservation_period_start,
+        s.reservation_period_end
+    FROM transactional.reservation r
+    JOIN transactional.run ru ON r.run_id = ru.run_id
+    JOIN transactional.showing s ON ru.showing_id = s.showing_id
+    JOIN transactional.play p ON s.play_id = p.play_id
+    JOIN transactional.theater t ON s.theater_id = t.theater_id
+    WHERE r.user_id = p_user_id
+    ORDER BY r.seat_id;
+END;
+$$;
