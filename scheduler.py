b4227492604b3f01580721ae93c5221c8edeaabf
Roemer Caliboso <roemergabrielcalib@gmail.com>
2025-11-28 08:31:14 +0800

    added scheduler for etl

---
+++ scheduler.py
@@ -0,0 +1,27 @@
+import time
+import schedule
+from sqlalchemy import text
+from app.lib.sql_controller import controller_analytical
+
+def run_etl_job():
+    print(f"[Scheduler] Initiating ETL.")
+    try:
+        with controller_analytical.engine.connect() as conn:
+            conn.execute(text("CALL analytical.batch_update_dw_incremental();"))
+            conn.commit()
+        print("[Scheduler] Success.")
+    except Exception as e:
+        print(f"[Scheduler] Failed: {e}")
+
+schedule.every().day.at("03:00").do(run_etl_job)
+
+if __name__ == "__main__":
+    print("[Scheduler] Service Started.")
+
+    print("[Scheduler] Initial run.")
+    run_etl_job() 
+
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
